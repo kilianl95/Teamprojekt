2ef2c98aa8db02a473191e2b48163f17879b222f
kilianl95 <79840202+kilianl95@users.noreply.github.com>
2021-10-04 14:53:54 +0200

    Noch mehr Umlaute gefixt und gitignore angepasst

--- src/main/java/gui/CnfReader.java
+++ src/main/java/gui/CnfReader.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -6,34 +7,36 @@ import java.util.regex.Pattern;
 
 
 public class CnfReader {
-	ArrayList<ArrayList<Integer>> knf;
-	
-	public CnfReader() {
-		knf = new ArrayList<>();
-	}
+    ArrayList<ArrayList<Integer>> knf;
+
+    public CnfReader() {
+        knf = new ArrayList<>();
+    }
 
     /**
      * Ueberprueft, ob der String die korrekte Syntax einer Klausel erfuellt.
+     *
      * @param line String, der ueberprueft werden soll
      * @returns true, falls alle chars des Strings einer Zahl zwischen 0 und 9, einem '-' oder einem
      * Leerzeichen entsprechen.
-     *
      */
-    public boolean isClause(String line)  {
+    public boolean isClause(String line) {
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
             if (!((c <= 57 && c >= 48) || c == '-' || c == ' ')) {
                 return false;
             }
 
-        } return true;
+        }
+        return true;
     }
 
     /**
      * Konvertiert den eingelesenen String in eine Klausel, d.h., die einzelnen Bestandteile des
      * Strings werden ueberprueft, ob sie in das entsprechende Muster passen und falls ja, in eine
      * ArrayList eingefuegt.
-     * @param line String, der sich aus durch Leerzeichen gertrennte Zahlen zusammensetzt
+     *
+     * @param line String, der sich aus durch Leerzeichen getrennte Zahlen zusammensetzt
      * @return Klausel - Eine ArrayList mit den Zahlen aus dem String
      */
     public ArrayList<Integer> makeLineToClause(String line) {
@@ -52,6 +55,7 @@ public class CnfReader {
 
     /**
      * Fuegt eine Klausel in die KNF ein.
+     *
      * @param s ArrayList mit Integern
      */
     public void addToKNF(ArrayList<Integer> s) {
@@ -66,22 +70,23 @@ public class CnfReader {
 
     /**
      * Baut die konjungierte Normalform (KNF) aus dem .cnf-File zusammen.
-     * @param reader BufferedReader - liest Zeile für Zeile
+     *
+     * @param reader BufferedReader - liest Zeile fuer Zeile
      * @return KNF - ArrayList mit Klauseln
      * @throws Exception falls ein Fehler im Ablauf auftritt
      */
-    public ArrayList<ArrayList<Integer>> buildKNF(BufferedReader reader)throws Exception{
+    public ArrayList<ArrayList<Integer>> buildKNF(BufferedReader reader) throws Exception {
         String line = reader.readLine();
 
-            while (line != null && !line.isEmpty()) {
+        while (line != null && !line.isEmpty()) {
 
-                if (isClause(line)) {
-                    addToKNF(makeLineToClause(line));
-                }
-                //read next line
-                line = reader.readLine();
+            if (isClause(line)) {
+                addToKNF(makeLineToClause(line));
             }
-            return knf;
+            //read next line
+            line = reader.readLine();
+        }
+        return knf;
     }
 
     ArrayList<ArrayList<Integer>> getKnf() {
--- src/main/java/gui/SatSolverRekursiv.java
+++ src/main/java/gui/SatSolverRekursiv.java
@@ -3,172 +3,176 @@ package gui;
 import java.util.ArrayList;
 
 public class SatSolverRekursiv {
-  /**
-   * Zaehlt die Rekursionsschritte.
-   */
-  public static int rek_counter = 0;
-  /**
-   * Dient zur Drosselung im Erklaermodus - wird true, falls der
-   * RadioButton 'Erklaermodus' aktiviert wird.
-   */
-  public static boolean delayMode = false;
-  /**
-   * Verzoegerungszeit für den Erklaermodus - laesst sich ueber den
-   * Slider festlegen.
-   */
-  public static long delayTime = 0;
-  /**
-   * Enthaelt die einzelnen Positionen für den Pseudo-Code (1-12).
-   */
-  public static int position = 0;
-  /**
-   * Notwendig für Position 4 und 7, da es hier zwei unterschiedliche Ausgaenge
-   * gibt und Markierung bei der Visualisierung sonst nicht richtig gesetzt wird.
-   */
-  public static int positionEnd = 0;
-  /**
-   * Gewaehltes Literal durch den Algorithmus. Hier: Es wird immer der naechstmoegliche
-   * Wert aus der ArrayList genommen. Static, weil der Controller für die TextArea darauf
-   * zugreifen muss.
-   */
-  public static int literal;
+    /**
+     * Zaehlt die Rekursionsschritte.
+     */
+    public static int rek_counter = 0;
+    /**
+     * Dient zur Drosselung im Erklaermodus - wird true, falls der
+     * RadioButton 'Erklaermodus' aktiviert wird.
+     */
+    public static boolean delayMode = false;
+    /**
+     * Verzoegerungszeit fuer den Erklaermodus - laesst sich ueber den
+     * Slider festlegen.
+     */
+    public static long delayTime = 0;
+    /**
+     * Enthaelt die einzelnen Positionen fuer den Pseudo-Code (1-12).
+     */
+    public static int position = 0;
+    /**
+     * Notwendig fuer Position 4 und 7, da es hier zwei unterschiedliche Ausgaenge
+     * gibt und Markierung bei der Visualisierung sonst nicht richtig gesetzt wird.
+     */
+    public static int positionEnd = 0;
+    /**
+     * Gewaehltes Literal durch den Algorithmus. Hier: Es wird immer der naechstmoegliche
+     * Wert aus der ArrayList genommen. Static, weil der Controller fuer die TextArea darauf
+     * zugreifen muss.
+     */
+    public static int literal;
 
-  private ArrayList<Integer> literalList = new ArrayList<>();
+    private ArrayList<Integer> literalList = new ArrayList<>();
 
 
-  public boolean DPLL(ArrayList<ArrayList<Integer>> F) throws InterruptedException {
+    public boolean DPLL(ArrayList<ArrayList<Integer>> F) throws InterruptedException {
 
-    position = 1;
+        position = 1;
 
-    for (int i = 0; i < F.size(); i++) {
+        for (int i = 0; i < F.size(); i++) {
 
-      if (F.get(i).size() == 1) {
-        int L = F.get(i).get(0);
-        F = simplify(F, L);
-        i = -1;
-      }
-    }
+            if (F.get(i).size() == 1) {
+                int L = F.get(i).get(0);
+                F = simplify(F, L);
+                i = -1;
+            }
+        }
 
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
-    position = 2;
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
+        position = 2;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
 
-    if (F.isEmpty()) {
+        if (F.isEmpty()) {
 
-      position = 3;
-      if(delayMode){
-        Thread.sleep(delayTime);
-      }
+            position = 3;
+            if (delayMode) {
+                Thread.sleep(delayTime);
+            }
 
-      return true;
-    }
+            return true;
+        }
 
-    position = 4;
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
+        position = 4;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
 
-    for (ArrayList<Integer> clause : F)
-      if (clause.isEmpty()) {
+        for (ArrayList<Integer> clause : F) {
+          if (clause.isEmpty()) {
 
-        position = 5;
-        if(delayMode){
-          Thread.sleep(delayTime);
+              position = 5;
+              if (delayMode) {
+                  Thread.sleep(delayTime);
+              }
+              positionEnd = 4;
+
+              return false;
+          }
+        }
+
+        positionEnd = 4;
+        position = 6;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
+
+        int L = F.get(0).get(0);
+        literal = L;
+
+        literalList.add(L);
+
+        position = 7;
+        if (delayMode) {
+            Thread.sleep(delayTime);
         }
         positionEnd = 4;
 
-        return false;
-      }
+        if (DPLL(simplify(F, L))) {
 
-    positionEnd = 4;
-    position = 6;
-    if(delayMode){
-      Thread.sleep(delayTime);
+            rek_counter++;
+            positionEnd = 7;
+            position = 8;
+            if (delayMode) {
+                Thread.sleep(delayTime);
+            }
+
+            return true;
+
+        } else {
+
+            rek_counter++;
+            positionEnd = 7;
+            position = 9;
+            if (delayMode) {
+                Thread.sleep(delayTime);
+            }
+
+            L = -L;
+            literalList.add(L);
+            literal = L;
+
+            return DPLL(simplify(F, L));
+        }
     }
 
-    int L = F.get(0).get(0);
-    literal = L;
+    public ArrayList<ArrayList<Integer>> simplify(ArrayList<ArrayList<Integer>> F, int L) throws InterruptedException {
 
-    literalList.add(L);
+        ArrayList<ArrayList<Integer>> FStrich = new ArrayList<>();
 
-    position = 7;
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
-    positionEnd = 4;
+        position = 10;
 
-    if (DPLL(simplify(F, L))) {
+        for (ArrayList<Integer> clause : F) {
+          if (!clause.contains(L)) {
+            FStrich.add(clause);
+          }
+        }
 
-      rek_counter++;
-      positionEnd = 7;
-      position = 8;
-      if(delayMode){
-        Thread.sleep(delayTime);
-      }
+        ArrayList<ArrayList<Integer>> FStrichStrich = new ArrayList<>();
 
-      return true;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
+        position = 11;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
 
-    } else {
+        for (ArrayList<Integer> clause : FStrich) {
+            ArrayList<Integer> clauseStrich = new ArrayList<>();
+            for (int literal : clause) {
+                if (literal != -L) {
+                  clauseStrich.add(literal);
+                }
+            }
+            FStrichStrich.add(clauseStrich);
+        }
 
-      rek_counter++;
-      positionEnd = 7;
-      position = 9;
-      if(delayMode){
-        Thread.sleep(delayTime);
-      }
+        position = 12;
+        if (delayMode) {
+            Thread.sleep(delayTime);
+        }
 
-      L = -L;
-      literalList.add(L);
-      literal = L;
-
-      return DPLL(simplify(F, L));
-    }
-  }
-
-  public ArrayList<ArrayList<Integer>> simplify(ArrayList<ArrayList<Integer>> F, int L) throws InterruptedException {
-
-    ArrayList<ArrayList<Integer>> FStrich = new ArrayList<>();
-
-    position = 10;
-
-    for (ArrayList<Integer> clause : F)
-      if (!clause.contains(L))
-        FStrich.add(clause);
-
-    ArrayList<ArrayList<Integer>> FStrichStrich = new ArrayList<>();
-
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
-    position = 11;
-    if(delayMode){
-      Thread.sleep(delayTime);
+        return FStrichStrich;
     }
 
-    for (ArrayList<Integer> clause : FStrich) {
-      ArrayList<Integer> clauseStrich = new ArrayList<>();
-      for (int literal : clause) {
-        if (literal != -L)
-          clauseStrich.add(literal);
-      }
-      FStrichStrich.add(clauseStrich);
+
+    public ArrayList<Integer> getLiteralList() {
+        return literalList;
     }
-
-    position = 12;
-    if(delayMode){
-      Thread.sleep(delayTime);
-    }
-
-    return FStrichStrich;
-  }
-
-
-  public ArrayList<Integer> getLiteralList(){
-    return literalList;
-  }
 
 }
